3a6753e6a77f19150bc4841200e816076020ccef
Edgar Fisher <fisher.edgar@gmail.com>
2016-07-12 15:38:35 +0300

    Initial commit

---
+++ lib/tracker_cli.rb
@@ -0,0 +1,9 @@
+require "tracker_cli/version"
+
+module TrackerCli
+  # Your code goes here...
+
+  def self.hello
+    p 'world'
+  end
+end
---
+++ lib/tracker_cli/version.rb
@@ -0,0 +1,3 @@
+module TrackerCli
+  VERSION = "0.1.0"
+end
